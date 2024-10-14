778e4c2da921ec15ddac02f8e14b3d0434ec0159
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-10-14 23:12:46 +0500

    Lesson-9: lc-1

--- Lesson-9/live-coding.py
+++ Lesson-9/live-coding.py
@@ -1,18 +1,40 @@
 # Тема 1. Продемонстрируйте и объясните в режиме live-coding:
 # - создание списка,
+fruits = ['apple', 'banana', 'peach']
+print(fruits)
+
 # - доступ к элементам списка,
+print(fruits[0])  # apple
+print(fruits[2])  # peach
+
 # - изменение значения элемента списка,
+fruits[1] = 'orange'
+print(fruits)
+
 # - получение среза,
+print(fruits[1:3])
+
 # - создание и работу с вложенными списками.
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+print(matrix[1])
+print(matrix[1][1])
+
+# сравнение списков
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+print(list1 == list2)
 
 
-
-
-
-
-
-
-
+# жизненные примеры
+users = [
+    ['user1', 'password', 'email1@example.com'],
+    ['user2', 'password', 'email2@example.com'],
+    ['user3', 'password', 'email3@example.com'],
+]
 
 
 # Тема 2. Продемонстрируйте и объясните в режиме live-coding:
