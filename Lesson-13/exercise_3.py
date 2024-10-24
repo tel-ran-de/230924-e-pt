96199d456751b8b19647c9f014d14af6855c2d95
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-10-25 01:04:44 +0500

    Lesson-13: exercise 3 40% done

--- Lesson-13/exercise_3.py
+++ Lesson-13/exercise_3.py
@@ -12,7 +12,7 @@
 # 3. Удалить товар.
 # 4. Обновить название товара, стоимость или количество.
 # 5. Найти товар по названию.
-# 6. Вывести список товаров меньше определнной стоимости.
+# 6. Вывести список товаров меньше определённой стоимости.
 # 7. Вывести список товаров меньше определенного количества.
 
 inventory = [
@@ -21,5 +21,77 @@ inventory = [
     {'product': "Keyboard", 'price': 30, 'count': 33},
     {'product': "Monitor", 'price': 20, 'count': 10}
 ]
+
+
+# функция для вывода списка товаров
+def show_inventory(product_list):
+    print('-------')
+    print('Список товаров')
+    for item in product_list:
+        print(f'Название: {item["product"]}, Цена: {item["price"]}, Количество: {item["count"]}')
+
+
+# функция для добавления нового товара
+def add_product(product_list, product, price, count):
+    pass
+
+
+# функция для удаления товара
+def remove_product(product_list, product):
+    pass
+
+
+# функция для обновления товара
+def update_product(product_list, product, price, count):
+    pass
+
+
+# функция для поиска товара по названию
+def find_product(product_list, product):
+    pass
+
+
+# функция для вывода товаров меньше определённой стоимости
+def show_products_below_price(product_list, price):
+    pass
+
+
+# функция для вывода товаров меньше определённого количества
+def show_products_below_count(product_list, count):
+    pass
+
+
+while True:
+    print('-------')
+    print('\nМеню:')
+    print('1. Показать список товаров.')
+    print('2. Добавить товар.')
+    print('3. Удалить товар.')
+    print('4. Обновить название товара, стоимость или количество.')
+    print('5. Найти товар по названию.')
+    print('6. Вывести список товаров меньше определённой стоимости.')
+    print('7. Вывести список товаров меньше определенного количества.')
+
+    choice = input('Выберите действие: ')
+
+    if choice == '1':
+        show_inventory(product_list=inventory)
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        pass
+    elif choice == '5':
+        pass
+    elif choice == '6':
+        pass
+    elif choice == '7':
+        pass
+    elif choice == '8':
+        print('Выход из программы')
+        break
+    else:
+        print('Неверный выбор. Пожалуйста, выберите снова.')
 
 
