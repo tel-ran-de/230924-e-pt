ac55a91f4850542f0094e76baa2254e016094280
DzimaStep <nifed3000@gmail.com>
2024-10-28 18:46:20 +0100

    Lesson-10/homework - done

--- Lesson-10/homework.py
+++ Lesson-10/homework.py
@@ -8,6 +8,17 @@
 # программа должна вывести сообщение "Вы ввели большее число" и продолжать запрашивать числа.
 # Если пользователь угадал, то программа должна вывести "Вы угадали число" и завершиться.
 
+# num = 30
+# while True:
+#     guess = int(input('Enter any number from 0 to 100: '))
+#     if guess > 30:
+#         print('You have entered too big number')
+#     elif guess < 30:
+#         print('You have entered too small number')
+#     else:
+#         print('You guessed the number!')
+#         break
+
 
 # Упражнение 2: Проверка пароля
 #
@@ -17,6 +28,27 @@
 # Если введен правильный пароль, программа должна выводить сообщение "Доступ разрешен" и завершаться.
 # Если после трех неправильных попыток пароль не введен правильно, программа должна выводить сообщение
 # "Превышено количество попыток" и завершаться.
+
+# attempts = 0
+# password = 'python123'
+# while True:
+#     enter = input('Password: ')
+#     if attempts == 3:
+#         print('You\'ve reached attempts limit')
+#         break
+#     elif ' ' in enter:
+#         print('Wrong password. Password should not nave spaces')
+#         attempts += 1
+#     elif enter != password:
+#         print('Wrong password')
+#         attempts += 1
+#     else:
+#         print('You\'ve guessed password')
+#         break
+#
+
+
+
 
 
 # Упражнение 3: Работа со списком покупок
@@ -28,6 +60,28 @@
 # "Формирование списка завершено" и завершаться. Если количество покупок ставится больше 6,
 # то программа должна вывести: “Превышен лимит покупок.” и завершиться.
 # Перед завершением программа должна выводить итоговый список покупок и общее количество элементов в нем.
+#
+# list = []
+# product = ''
+# counter = 0
+# while product != 'stop' or counter != 6:
+#     product = input('Enter product name: ')
+#     if product == 'stop':
+#         print('List has been created')
+#         print(list)
+#         break
+#     elif counter == 6:
+#         print('Products limit reached')
+#         print(list)
+#         break
+#     if product in list:
+#         print('You already have this product')
+#     else:
+#         list.append(product)
+#         print('Product has been added')
+#         counter += 1
+# else:
+#     print(list)
 
 
 # Тема: Цикл for
@@ -37,12 +91,34 @@
 # Напишите программу, которая принимает строку от пользователя и подсчитывать количество гласных букв (a, e, i, o, u)
 # в этой строке.Используйте цикл for и условие if.
 
+# vowels = list('aeiou')
+# word = input('Введите слово: ')
+# counter=0
+# for char in word:
+#     if char in vowels:
+#         counter += 1
+# print('Vowels amount: ', counter)
+
+
+
 
 # Упражнение 2: Генерация и вывод последовательности чисел
 #
 # Напишите программу, которая генерит и выводит последовательность чисел от 1 до 20,
 # но выводит "Fizz" вместо чисел, кратных 3, "Buzz" вместо чисел, кратных 5, и "FizzBuzz"
 # вместо чисел, кратных как 3, так и 5. Используйте цикл for и функцию range.
+
+
+# for digit in range(1, 21):
+#     if digit % 3 == 0 and digit % 5 == 0:
+#         print('FizzBuzz')
+#     if digit % 3 == 0:
+#         print('Fizz')
+#     elif digit % 5 == 0:
+#         print('Buzz')
+#     else:
+#         print(digit)
+
 
 
 # Проект 1: Управление библиотекой
@@ -53,10 +129,12 @@
 #
 # Требования: Реализуйте работу всех пунктов меню.
 #
+# Продолжите программу ниже.
+
+
 # library = [["Война и мир", "Толстой", "в наличии"],
-#            ["Преступление и наказание", "Достоевский", "выдана"],
-#            ["Мастер и Маргарита", "Булгаков", "в наличии"]]
-#
+#             ["Преступление и наказание", "Достоевский", "выдана"],
+#             ["Мастер и Маргарита", "Булгаков", "в наличии"]]
 # while True:
 #     print("\nМеню")
 #     print("1. Показать список всех книг")
@@ -66,8 +144,39 @@
 #     print("5. Показать книги определенного автора")
 #     print("6. Показать книги с определенным статусом")
 #     choice = input("Выберите действие, введя его номер: ")
-#
-#     # Продолжите программу ниже.
+#     if choice == '1':
+#         for number, book in enumerate(library):
+#             print(f'{number + 1}. {book[0]}, {book[1]}, {book[2]}')
+#     elif choice == '2':
+#         new_book = input('Название книги: ')
+#         book_auth = input('Автор: ')
+#         book_status = input('Наличие:')
+#         library.append([new_book, book_auth, book_status])
+#         print('Книга добавлена')
+#     elif choice == '3':
+#         del_book = input('Название книги, чтобы удалить: ')
+#         for book in library:
+#             if book[0] == del_book:
+#                 library.remove(book)
+#                 print(f'Книга "{del_book}" удалена')
+#     elif choice == '4':
+#         book_title = input('Введите книгу, чтобы изменить статус: ')
+#         for book in library:
+#             if book[0] == book_title:
+#                 book_status = input('в наличии/выдана: ')
+#                 book[2] = book_status
+#                 print('Статус изменен')
+#     elif choice == '5':
+#         srch_auth = input('Введите автора книг: ')
+#         for number, book in enumerate(library):
+#             if book[1] == srch_auth:
+#                 print(f'{number + 1}. {book[0]}, {book[2]}')
+#     elif choice == '6':
+#         srch_status = input('Введите статус книг: ')
+#         for number, book in enumerate(library):
+#             if book[2] == srch_status:
+#                 print(f'{number + 1}. {book[0]}, {book[1]}')
+
 
 
 # Проект 2: Анализ посещаемости на сайте
@@ -76,5 +185,42 @@
 # за каждый день недели, определять дни с наибольшей и наименьшей посещаемостью, рассчитывать среднюю посещаемость
 # за неделю и выводить дни с посещаемостью выше среднего.
 #
-# days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
-# visits = []
+
+# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# while True:
+#     print("\nMenu")
+#     print('1. Enter amount of visits: ')
+#     print('2. Day with max visits: ')
+#     print('3. Day with min visits: ')
+#     print('4. Average attendance: ')
+#     choice = input('Select action: ' )
+#     if choice == '1':
+#         visits = []
+#         for day in days:
+#             visit = int(input(f'Enter number of visits for {day}: '))
+#             visits.append(visit)
+#     elif choice == '2':
+#         max_visits = max(visits)
+#         max_day = []
+#         for i, visit in enumerate(visits):
+#             if visits[i] == max(visits):
+#                 max_day.append(days[i])
+#         print(f'Day with max visits: {max_visits} on {', '.join(max_day)}')
+#     elif choice == '3':
+#         min_visits = min(visits)
+#         min_day = []
+#         for i, visit in enumerate(visits):
+#             if visits[i] == min(visits):
+#                 min_day.append(days[i])
+#         print(f'Day with min visits: {min_visits} on {', '.join(min_day)}')
+#     elif choice == '4':
+#         avrg_visits = sum(visits) / len(visits)
+#         print(f'Average attendance: {avrg_visits}')
+#         above_avrg_days = []
+#         for i, visit in enumerate(visits):
+#             if visit > avrg_visits:
+#                 above_avrg_days.append(days[i])
+#         print(f'Days with attendance above average: {', '.join(above_avrg_days)}')
+#     else:
+#         print('Invalid choice. Try again.')
+#         break
