8aacbd7632d48cef259af1d10bf7ed90adaf5cc8
Igor Pronin <igp78@mail.ru>
2024-10-29 13:19:13 +0100

    задача 2 решена

--- Lesson-12/homework.py
+++ Lesson-12/homework.py
@@ -54,11 +54,33 @@ print("------------------------")
 # 5. Добавьте новый товар "Dates" с количеством 15 и ценой 4.
 # 6. Выведите общую стоимость всех товаров (количество * цена для каждого товара и сумма этих значений).
 #
-# inventory = {
-#     "Apples": {"quantity": 50, "price": 2},
-#     "Bananas": {"quantity": 30, "price": 1},
-#     "Cherries": {"quantity": 20, "price": 3},
-# }
+inventory = {
+    "Apples": {"quantity": 50, "price": 2},
+    "Bananas": {"quantity": 30, "price": 1},
+    "Cherries": {"quantity": 20, "price": 3},
+}
+print("---------------------")
+print("1. Название всех товаров")
+for name in inventory:
+    print(f"{name}")
+print("----------------------")
+print("2. Увеличить количество яблок на 10")
+inventory["Apples"]["quantity"] += 10
+print("----------------------")
+print("3. Изменить цену бананов на 1,5")
+inventory["Bananas"]["price"] = 1.5
+print("----------------------")
+print("4. Удалить товар Черри")
+del inventory["Cherries"]
+print("----------------------")
+print("5. Добавить новый товар - финики количеством 15 и ценой 4")
+inventory["Dates"] = {"quantity": 15, "price": 4}
+print("----------------------")
+
+total_cost = 0
+for item, details in inventory.items():
+    total_cost += details["quantity"] * details["price"]
+print(f"Общая стоимость товаров: {total_cost}")
 
 
 # Тема: кортежи и множества.
