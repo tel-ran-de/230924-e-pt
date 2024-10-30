82a4babaf2f3cccccc7befce0e6cfedf853a8be9
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-10-30 19:06:54 +0100

    синтаксическая ошибка

--- Lesson-13/homework.py
+++ Lesson-13/homework.py
@@ -1,16 +1,29 @@
 # Задача 1: Анализ чисел
 # Напишите функцию `analyze_numbers(numbers)`, которая принимает список чисел
 # и возвращает кортеж из трех значений: сумма всех чисел, среднее значение и количество четных чисел.
-#
-# numbers = [1, 2, 3, 4, 5, 6]
+#from hashlib import algorithms_available
+print("------------------------Задача 1: Анализ чисел.---------------------------")
+def analyze_numbers(numbers):
+    sum_numbers = sum(numbers)
+    average_num = sum_numbers / len(numbers)
+    count_of_even = sum(1 for num in numbers if num % 2 ==0)
+    return sum_numbers, average_num, count_of_even
+numbers = [1, 2, 3, 4, 5, 6]
+print(analyze_numbers(numbers))
+
 # Вывод функции: (21, 3.5, 3)
-
-
 # Задача 2: Работа со строками
 # Напишите функцию `analyze_strings(strings)`, которая принимает список строк
 # и возвращает кортеж из трех значений: самая длинная строка, самая короткая строка и количество строк, содержащих букву "a"..
 #
-# strings = ["apple", "banana", "cherry", "date"]
+print("-----------------------Задача 2: Работа со строками.----------------------")
+def analyze_strings(strings):
+    long_str = max(strings, key=len)
+    short_str = min(strings, key=len)
+    count_str_with_a = sum(1 for string in strings if 'a' in string)
+    return long_str, short_str, count_str_with_a
+strings = ["apple", "banana", "cherry", "date"]
+print(analyze_strings(strings))
 # Вывод функции: ('banana', 'date', 3)
 
 
@@ -18,24 +31,39 @@
 # Напишите функцию `analyze_salaries(employees)`, которая принимает словарь сотрудников и
 # возвращает кортеж из трех значений: средняя зарплата, максимальная зарплата и имя сотрудника с максимальной зарплатой.
 #
-# employees = {"Alice": 5000, "Bob": 7000, "Charlie": 6000}
+print("-------------------Задача 3: Обработка словаря сотрудников.----------------")
+def analyze_salaries(employees):
+    average_salary = sum(employees.values()) / len(employees)
+    max_salary = max(employees.values())
+    max_salary_employee = max(employees, key=employees.get)
+    return average_salary, max_salary, max_salary_employee
+employees = {"Alice": 5000, "Bob": 7000, "Charlie": 6000}
+print(analyze_salaries(employees))
 # Вывод функции: (6000.0, 7000, 'Bob')
 
 
 # Задача 4: Фильтрация списка
 # Напишите функцию `filter_numbers(numbers)`, которая принимает список чисел и
 # возвращает кортеж из двух списков: четные числа и нечетные числа.
-#
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("------------------------Задача 4: Фильтрация списка.-----------------------")
+def filter_numbers(numbers):
+    even_numbers = [num for num in numbers if num % 2 == 0]
+    odd_numbers = [num for num in numbers if num % 2 != 0]
+    return even_numbers, odd_numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(filter_numbers(numbers))
 # Вывод функции: ([2, 4, 6, 8, 10], [1, 3, 5, 7, 9])
 
 
 # Задача 5: Генерация словаря
 # Напишите функцию `create_dict(keys, values)`, которая принимает два списка: ключи и значения,
 # и возвращает словарь, где ключи из первого списка, а значения из второго.
-#
-# keys = ["name", "age", "city"]
-# values = ["Alice", 30, "New York"]
+print("------------------------Задача 5: Генерация словаря.-----------------------")
+def create_dict(keys, values):
+
+keys = ["name", "age", "city"]
+values = ["Alice", 30, "New York"]
+print(create_dict(keys, values))
 # Вывод функции: {'name': 'Alice', 'age': 30, 'city': 'New York'}
 
 
@@ -76,7 +104,7 @@
 # 3. Удалить товар.
 # 4. Обновить название товара, стоимость или количество.
 # 5. Найти товар по названию.
-# 6. Вывести список товаров меньше определнной стоимости.
+# 6. Вывести список товаров меньше определенной стоимости.
 # 7. Вывести список товаров меньше определенного количества.
 
 # inventory = [
