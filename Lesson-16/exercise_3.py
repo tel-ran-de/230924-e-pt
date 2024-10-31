bb9c1a272ee386ec3ec5a02b2acfd7e586bb2af7
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-10-31 22:55:51 +0100

    практика3 урока 16 выполнена

--- Lesson-16/exercise_3.py
+++ Lesson-16/exercise_3.py
@@ -9,8 +9,20 @@
 #
 # print(get_greeting("Алиса"))
 # # Вывод: ПРИВЕТ, АЛИСА
+print("--------------------------------------------------------------")
+def to_upper(func):
+    def decorator(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result.upper()
 
+    return decorator
 
+@to_upper
+def get_greeting(name):
+    return f"Привет, {name}"
+
+print(get_greeting("Алиса"))
+print("--------------------------------------------------------------")
 # 2. Создайте декоратор limit_calls, который ограничивает количество вызовов функции заданным числом.
 # Если функция вызывается больше разрешенного числа раз, выводите сообщение об ошибке.
 #
@@ -26,3 +38,29 @@
 # # Вывод: Привет, Чарли!
 # say_hello("Дейв")
 # # Вывод: Ошибка: функция say_hello может быть вызвана не более 3 раз
+
+def limit_calls(max_call):
+    def decorator(func):
+        i = 0
+        def wrapper(*args, **kwargs):
+            nonlocal  i
+            if i < max_call:
+                i += 1
+                return func (*args, **kwargs)
+            else:
+                print(f"Ошибка: функция {func.__name__} может быть вызвана не более {max_call} раз")
+        return wrapper
+    return decorator
+
+@limit_calls(3)
+def say_hello(name):
+    print(f"Привет, {name}!")
+
+say_hello("Алиса")
+# Вывод: Привет, Алиса!
+say_hello("Боб")
+# Вывод: Привет, Боб!
+say_hello("Чарли")
+# Вывод: Привет, Чарли!
+say_hello("Дейв")
+# Вывод: Ошибка: функция say_hello может быть вызвана не более 3 раз
