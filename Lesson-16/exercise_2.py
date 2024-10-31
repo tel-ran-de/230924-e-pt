61c8dcdfb6b38902dc70bf1fb1e5d91a671181c8
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-10-31 22:55:47 +0100

    практика2 урока 16 выполнена

--- Lesson-16/exercise_2.py
+++ Lesson-16/exercise_2.py
@@ -1,5 +1,6 @@
 # Дополнительная практика
-
+from itertools import count
+from pstats import count_calls
 
 # 1. Создайте декоратор requires_auth, который проверяет наличие определенного флага authenticated и выполняет
 # функцию только если этот флаг установлен в True.
@@ -12,27 +13,50 @@
 #
 # secret_function()
 # Вывод: Доступ запрещен: пользователь не аутентифицирован
-#
-# authenticated = True
-# secret_function()
+print("--------------------------------------------------------------")
+authenticated = False
+
+def requires_auth(func):
+    def decorator(*args, **kwargs):
+        if authenticated:
+            return func(*args, **kwargs)
+        else:
+            print("Доступ запрещен: пользователь не аутентифицирован")
+
+    return decorator
+
+@requires_auth
+def secret_function():
+    print("Секретная информация")
+
+secret_function()
+
+authenticated = True
+secret_function()
 # Вывод: Секретная информация
 
-
+print("--------------------------------------------------------------")
 # 2. Создайте декоратор call_counter, который отслеживает количество вызовов декорируемой функции и
 # выводит это количество при каждом вызове.
+def call_counter(func):
+    def decorator(*args, **kwargs):
+        decorator.call_count += 1
+        print(f"Функция {func.__name__} вызвана {decorator.call_count} раз(а).")
+        return func(*args, **kwargs)
+    decorator.call_count = 0
+    return decorator
+
+@call_counter
+def greet(name):
+    print(f"Привет, {name}!")
 #
-# @call_counter
-# def greet(name):
-#     print(f"Привет, {name}!")
-#
-# greet("Алиса")
+greet("Алиса")
 # # Вывод:
 # # Функция greet вызвана 1 раз(а)
 # # Привет, Алиса!
 #
-# greet("Боб")
+greet("Боб")
 # # Вывод:
 # # Функция greet вызвана 2 раз(а)
 # # Привет, Боб!
-
-
+greet("Виктор")
