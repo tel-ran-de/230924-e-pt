b3a0ed749a691cf67905994666090911cc66eaf5
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-03 23:45:37 +0100

    выполнение ДЗ урок 14 без трех заданий

--- Lesson-14/homework.py
+++ Lesson-14/homework.py
@@ -1,29 +1,57 @@
 # Тема: Упаковка аргументов с помощью *args, **kwargs и распаковка через * и **
+from itertools import count
+
 
 # 1. Напишите функцию sum_all, которая принимает произвольное количество числовых аргументов
 # с помощью *args и возвращает их сумму.
-
+def sum_all(*args):
+    return sum(args)
+print(sum_all(13, 2, 4, 5))
 
 # 2. Напишите функцию combine_lists, которая принимает несколько списков в качестве аргументов с помощью *args
 # и возвращает один объединённый список.
+
+def combine_lists(*args, **kwargs):
+    return [i for j in args for i in j]
+print(combine_lists([1, 2, 3],['a', 'b', 'c'],[1,'a', 4]))
+
 
 
 # 3. Напишите функцию print_details, которая принимает два аргумента name и age.
 # Затем создайте словарь с ключами name и age, распакуйте его и передайте в функцию print_details.
 
+def print_details(**kwargs):
+    return kwargs
+print(print_details(name="Viktor", age = 43))
 
 # 4. Напишите функцию filter_numbers, которая принимает произвольное количество числовых аргументов с помощью *args
 # и возвращает список только тех чисел, которые больше 10.
 
+def filter_numbers(*args):
+    return [i for i in args if i > 10]
+print(filter_numbers(12, 10.2, 3, 17, 21))
 
 # Тема: Глобальные и локальные переменные. Вложенные функции и замыкания.
-
+print("-------------Глобальные и локальные переменные. Задание 1.---------------")
 # 1. Напишите функцию increment_global, которая увеличивает значение глобальной переменной counter на 1 каждый раз,
 # когда она вызывается.
 # increment_global()
 # print(counter)  # Вывод: 1
 # increment_global()
 # print(counter)  # Вывод: 2
+counter = 0
+def increment_global():
+    global counter
+    counter += 1
+    return counter
+increment_global()
+increment_global()
+increment_global()
+increment_global()
+print(increment_global())
+print(increment_global())
+print(increment_global())
+print(increment_global())
 
 
 # 2. Напишите функцию outer, которая содержит внутреннюю функцию inner. Внутренняя функция должна увеличивать
@@ -31,7 +59,22 @@
 # counter = outer()
 # print(counter())  # Вывод: 1
 # print(counter())  # Вывод: 2
+print("-------------Задание 2.---------------")
+def outer():
+    count = 0
+    def inner():
+        nonlocal count
+        count += 1
+        return count
+    return inner
 
+counter1 = outer()  #замыкание
+print(counter1())
+print(counter1())
+print(counter1())
+print(counter1())
+print(counter1())
+print(counter1())
 
 # 3. Напишите функцию make_multiplier, которая принимает аргумент factor. Внутри этой функции создайте и
 # верните функцию multiplier, которая умножает свой аргумент на factor.
@@ -39,33 +82,78 @@
 # print(mult_by_2(5))  # Вывод: 10
 # mult_by_3 = make_multiplier(3)
 # print(mult_by_3(5))  # Вывод: 15
-
+print("-------------Задание 3.---------------")
+def make_multiplier(factor):
+    def multiplier(num):
+        return factor*num
+    return multiplier
+mult_by_2 = make_multiplier(6)
+print(mult_by_2(11))  # Вывод: 10
+mult_by_3 = make_multiplier(10)
+print(mult_by_3(45))  # Вывод: 15
 
 # 4. Напишите функцию make_prefixer, которая принимает строку prefix и возвращает внутреннюю функцию prefixer.
 # Внутренняя функция должна добавлять prefix к любому переданному ей аргументу.
 # add_hello = make_prefixer("Hello, ")
 # print(add_hello("Alice"))  # Вывод: Hello, Alice
 # print(add_hello("Bob"))    # Вывод: Hello, Bob
-
+print("-------------Задание 4.---------------")
+def make_prefixer(prefix):
+    def prefixer(str):
+        return prefix+str
+    return prefixer
+add_hello = make_prefixer("Hello, ")
+print(add_hello("Alice"))
+print(add_hello("Bob"))
 
 # Тема: Дополнительная практика
-
+print("-------------Доп. Задание 1.---------------")
 # 1. Напишите функцию create_user, которая принимает параметры username, email
 # и произвольное количество дополнительных данных с помощью **kwargs.
 # Функция должна возвращать словарь с информацией о пользователе.
+# def create_user(username):
+#     def email(email):
+#     return f'{username}:, +' '+{email}:'
+# return email
+#
+# hello_greeter = make_greeter('Hello')
+# print(hello_greeter('Alice'))
 
+# hi_greeter = make_greeter('Hi')
+# print(hi_greeter('Bob'))
+#
+#
+# merhaba_greeter = make_greeter('Merhaba')
+# print(merhaba_greeter('Metin'))
+def create_user(**kwargs):
+    return kwargs
+print(create_user(username="Nikita", email="ttt", age=30, height=100))
+
+
+
+print("-------------Доп. Задание 2.---------------")
 # 2. Напишите функцию make_replacer, которая принимает два аргумента old и new. Внутри этой функции создайте
 # и верните функцию replacer, которая заменяет все вхождения old на new в переданной ей строке.
 # replace_a_with_o = make_replacer("a", "o")
 # print(replace_a_with_o("banana"))  # Вывод: bonono
 # print(replace_a_with_o("apple"))   # Вывод: opple
 
+def make_replacer(old, new):
+    def replacer(lst):
+        return lst.replace(old, new)
+    return replacer
+
+replace_a_with_o = make_replacer("a", "o")
+print(replace_a_with_o("baanaanaa"))  # Вывод: bonono
+print(replace_a_with_o("aapplae"))   # Вывод: opple
 
 # 3. Напишите функцию make_suffixer, которая принимает строку suffix и возвращает внутреннюю функцию suffixer.
 # Внутренняя функция должна добавлять suffix к любому переданному ей аргументу.
 # add_exclamation = make_suffixer("!")
 # print(add_exclamation("Hello"))  # Вывод: Hello!
 # print(add_exclamation("Wow"))    # Вывод: Wow!
+print("-------------Доп. Задание 3.---------------")
+
 
 
 # 4. Напишите функцию make_case_changer, которая принимает аргумент case (значения могут быть "upper" или "lower").
