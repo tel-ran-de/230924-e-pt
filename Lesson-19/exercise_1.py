189e0a404acdd90fe4d4f576c159999b3450cb5b
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-07 21:04:49 +0100

    перезапись

--- Lesson-19/exercise_1.py
+++ Lesson-19/exercise_1.py
@@ -6,17 +6,35 @@
 # 3. Прочитайте первые 10 символов файла и выведите их на экран.
 # 4. Прочитайте одну строку из файла и выведите ее на экран.
 # 5. Прочитайте все строки файла и выведите их на экран.
-
-file_r = open("data.txt", "r")
-print(file_r)
-
+file_r = open('text_files/data.txt', 'r')
+content = file_r.read()
+print(content)
+print("------------------")
+file_r.seek(0)
+content = file_r.read(10)
+print(content)
+print("------------------")
+file_r.seek(0)
+content = file_r.readline()
+print(content)
+print("------------------")
+file_r.seek(0)
+content = file_r.readlines()
+print(*content)
+file_r.close()
 # Задание 2: Запись данных в файл
 # 1. Откройте (создайте) файл `output.txt` для записи.
 # 2. Запишите в файл строку "Hello, World!".
 # 3. Запишите в файл список строк: ["This is line 1\n", "This is line 2\n"].
 # 4. Закройте файл.
 # 5. Откройте файл `output.txt` для чтения и выведите его содержимое на экран.
-
+print("----------Задание 2.--------")
+file_make_output = open('text_files/output.txt', 'w')
+file_make_output.write('Hello, World!\n')
+lines = ["This is line 1\n", "This is line 2\n"]
+file_make_output.writelines(lines)
+print(*lines)
+file_make_output.close()
 
 # Задание 3: Добавление данных в файл
 # 1. Откройте (создайте) файл `log.txt` для добавления данных.
@@ -24,8 +42,13 @@ print(file_r)
 # 3. Добавьте список строк ["Log entry 1\n", "Log entry 2\n"] в конец файла.
 # 4. Закройте файл.
 # 5. Откройте файл `log.txt` для чтения и выведите его содержимое на экран.
-
-
+print("----------Задание 3.--------")
+file_make_log = open('text_files/log.txt', 'w')
+file_make_log.write("New log entry\n")
+lines = ["Log entry 1\n", "Log entry 2\n"]
+file_make_log.writelines(lines)
+print(*lines)
+file_make_log.close()
 # Задание 4: Работа с указателем
 # 1. Откройте (создайте) файл `pointer_example.txt` для чтения и записи.
 # 2. Запишите в файл строку "Python File Handling\n".
