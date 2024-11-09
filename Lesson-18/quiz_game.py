b6cfebc0ab84dab6ec289f19d20ff554989ad868
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-09 11:12:00 +0100

    игра Викторина выполнена

--- Lesson-18/quiz_game.py
+++ Lesson-18/quiz_game.py
@@ -30,24 +30,46 @@
 # — 3. Мюнхен
 
 # def game_quiz(*args, **kwargs):
-dict_capital = {
-    "Столица Франции": {"1": "Лондон", "2": "Берлин", "3": "Париж"},
-    "Столица Германии": {"1": "Лондон", "2": "Берлин", "3": "Венеция"},
-    "Столица США": {"1": "Нью-Йорк", "2": "Лос-Анджелес", "3": "Вашингтон"},
-    "Столица Греции": {"1": "Афины", "2": "Стамбул", "3": "Киев"},
-    "Столица Норвегии": {"1": "Осло", "2": "Порту", "3": "Мюнхен"}
-}
-for question, list_city in dict_capital.items():
-    print(f"{question}: ")
-    # print(end=" ")
-    for y, w in list_city.items():
-        print(f"{y}. {w}")
-    # user_answer = input(print("Введите вариант ответа: "))
-    # print(f"{list_city}", end="\n")
-    print("-------------------------------------------")
-    user_answer = int(input("Введите вариант ответа: "))
-    print("-------------------------------------------")
-    for e, p in enumerate(question):
-        if user_answer == question[e]:
-            print("Правильно")
+def quiz_game(question_country, list_city, correct_answer):
+    while True:
+        print(question_country)
+        print(28 * "-")
+        for num, city in enumerate(list_city, start=1):
+            print(f"{num}. {city}")
+        try:
+            print(28 * "-")
+            user_answer = int(input("Введите вариант ответа: "))
+            print(28 * "-")
+            if 1 <= user_answer <= len(list_city):
+                if user_answer == correct_answer:
+                    print("Ответ правильный.\n")
+                    return True
+                else:
+                    print("Ответ неправильный.\n")
+                    return False
+            else:
+                print("Неверный ввод. Выберите число от 1 до", len(list_city), "\n")
+        except ValueError:
+            print("Неверный ввод. Введите число.\n")
+
+def main():
+    dict_capital = [
+        ("Столица Франции?", ["Лондон", "Берлин", "Париж"], 3),
+        ("Столица Германии?", ["Лондон", "Берлин", "Венеция"], 2),
+        ("Столица США?", ["Нью-Йорк", "Лос-Анджелес", "Вашингтон"], 3),
+        ("Столица Греции?", ["Афины", "Стамбул", "Киев"], 1),
+        ("Столица Норвегии?", ["Осло", "Порту", "Мюнхен"], 1)
+    ]
+
+    correct_answers = 0
+    print("----- Игра: Викторина. -----")
+    for question_country, list_city, correct_answer in dict_capital:
+        if quiz_game(question_country, list_city, correct_answer):
+            correct_answers += 1
+
+    print(f"Игра завершена. Вы дали {correct_answers} правильных ответа из 5.")
+
+if __name__ == "__main__":
+    main()
+
 
