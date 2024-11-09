fb757d45da7721377e8349b756a3203f0293699c
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-09 12:03:17 +0100

    предложение Евгения для старта

--- Lesson-18/test_miniproject.py
+++ Lesson-18/test_miniproject.py
@@ -1,18 +1,46 @@
-dict_capital = {
-                    "Столица Франции":{"1": "Лондон", "2": "Берлин", "3": "Париж"},
-                    "Столица Германии":{"1": "Лондон", "2": "Берлин", "3": "Венеция"},
-                    "Столица США":{"1": "Нью-Йорк", "2": "Лос-Анджелес", "3": "Вашингтон"},
-                    "Столица Греции":{"1": "Афины", "2": "Стамбул", "3": "Киев"},
-                    "Столица Норвегии":{"1": "Осло", "2": "Порту", "3": "Мюнхен"}
-                    }
-for x, i in dict_capital.items():
-    print(f"{x}:")
+# Игра: Сапёр рекомендации Евгений
+pattern_list = [
+    ["-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-"]
+]
 
-    for y, w in i.items():
-        print(f"{y}. {w}")
-    a = int(input())
+import random
 
 
-    print(end = " ")
-    # print(f"{i}", end = " ")
+for row in pattern_list:
+    print(" ".join(row))
+print()
+
+game_coord = []
+
+for v in range(0, 5):
+    x1 = random.randrange(0, 5)
+    y1 = random.randrange(0, 5)
+    pattern_list[int(x1)][int(y1)] = 'X'
+    pl = [x1, y1]
+    game_coord.append(pl)
+
+for row in pattern_list:
+    print(" ".join(row))
+
+
+while True:
+    user = True
+
+    a = input()
+    c = a.split()
+    print(a, c)
+    print(c)  # list
+
+    x, y = c[0], c[1]
+    print("Координаты : x:", int(x) + 1, " y:", int(y) + 1)
+
+    # if user == True:
+    #    pattern_list[int(x)][int(y)] = 'X'
+
+    for row in pattern_list:
+        print(" ".join(row))
     print()
