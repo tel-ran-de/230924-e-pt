90c7270ddc86b14f8abfa2f11481a79775f7a70b
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-10 21:12:56 +0100

    добавил последнюю игру

--- Lesson-18/main.py
+++ Lesson-18/main.py
@@ -10,6 +10,7 @@ from quiz_game import quiz_game_main
 from rock_paper_scissors import game_paper_scissors
 from hangman import game_hangman
 from text_adventure import text_quest
+from minesweeper import minesweeper
 
 
 def main_menu():
@@ -40,6 +41,7 @@ def main_menu():
             text_quest()
             print("")
         elif choice == '6':
+            minesweeper()
             print("")
         elif choice == '0':
             print("Выход из программы.")
