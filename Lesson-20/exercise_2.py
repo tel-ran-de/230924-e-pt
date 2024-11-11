b416d85d7d8591f8d881c1299c2b7ff4b815d16f
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-11 19:40:57 +0100

    очистил файл от информации, только задания

--- Lesson-20/exercise_2.py
+++ Lesson-20/exercise_2.py
@@ -2,38 +2,15 @@
 
 # Задача 1. Допишите код ниже.
 
-import math
-
-def calculate_square_root(number):
-    if number < 0:
-        raise ValueError('Число должно быть не отрицательным')
-    return math.sqrt(number)
-
-def safe_calculate_square_root(number):
-    try:
-        result = calculate_square_root(number)
-        print(f"Квадратный корень из {number} равен {result}")
-    except ValueError as e:
-        print(f"Ошибка: {e}")
 
 # Тесты функции
-safe_calculate_square_root(25)  # Ожидаемый результат: Квадратный корень из 25 равен 5.0
-safe_calculate_square_root(-9)  # Ожидаемый результат: Ошибка: Число должно быть положительным
+# safe_calculate_square_root(25)  # Ожидаемый результат: Квадратный корень из 25 равен 5.0
+# safe_calculate_square_root(-9)  # Ожидаемый результат: Ошибка: Число должно быть положительным
 
 
 # Задача 2. Допишите код ниже.
-def convert_to_number(string):
-    if not string.isdigit():
-        raise ValueError('Введёное значение не является числом')
-    return int(string)
 
-def safe_convert_to_number(string):
-    try:
-        number = convert_to_number(string)
-        print(f"Конвертированное число: {number}")
-    except ValueError as e:
-        print(f"Ошибка: {e}")
 
 # Тесты функции
-safe_convert_to_number("123")  # Ожидаемый результат: Конвертированное число: 123
-safe_convert_to_number("abc")  # Ожидаемый результат: Ошибка: Введенное значение не является числом
+# safe_convert_to_number("123")  # Ожидаемый результат: Конвертированное число: 123
+# safe_convert_to_number("abc")  # Ожидаемый результат: Ошибка: Введенное значение не является числом
