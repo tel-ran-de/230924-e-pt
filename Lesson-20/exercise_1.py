4f28c019bebf8b4265640baf90fc0ba5f00bab0d
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-11-11 23:37:50 +0500

    Lesson-20: e1, e2, e3 перезаписаны!

--- Lesson-20/exercise_1.py
+++ Lesson-20/exercise_1.py
@@ -5,41 +5,11 @@
 # Обработайте исключения для случаев, когда:
 # - деление на ноль
 # - ввод не числовых значений.
-def divide_numbers(x, y):
-    try:
-        result = x / y
-    except ZeroDivisionError as e:
-        return f'На ноль делить нельзя {e}'
-    except TypeError as e:
-        return f'Введено не число {e}'
-    except ValueError as e:
-        return f'Введено не число {e}'
-    # except:
-    #     return 'Что-то пошло не так!'
-    else:
-        return result
-
-
-print(divide_numbers(1, 0))
-print(divide_numbers(1, '0'))
-print(divide_numbers(1, None))
-print(divide_numbers(144, 12))
 
 
 # Задача 2: Обработка пользовательского ввода
 # Напишите программу, которая запрашивает у пользователя ввод числа и выводит его квадрат.
 # Обработайте исключения для случаев, когда ввод не является числом.
-def square_number():
-    try:
-        number = float(input('Введите число: '))
-        result = number ** 2
-    except ValueError:
-        return "Введённое значение не является числом"
-    else:
-        return f'Квадрат числа {number} равен {result}'
-
-
-print(square_number())
 
 
 # Задача 3. Вернитесь к задачам предыдущего урока из файла exercise_1 и добавьте в решение обработку возможных ошибок,
