cc68fa1d82997071c5403d6e71c777a712e6c9b3
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-13 20:21:43 +0100

    1 практика выполнена

--- Lesson-21/exercise_1.py
+++ Lesson-21/exercise_1.py
@@ -3,12 +3,35 @@
 # Задание 1: Напишите функцию, которая создает итератор, возвращающий числа от 1 до заданного числа `n`.
 # Обработайте исключение StopIteration
 
+def create_iterator(n):
+    for i in range(1, n+1):
+        yield i
+result = create_iterator(9)
+try:
+    while True:
+        print(next(result))
+except StopIteration:
+    print("Iteration complete")
 
 # Задание 2: Напишите выражение-генератор, которое возвращает квадраты чисел от 0 до 10.
 # Обработайте исключение StopIteration
 
+squares_engine = (x ** 2 for x in range(0, 11))
+try:
+    for elem in squares_engine:
+        print(elem)
+except StopIteration:
+    print("Iteration complete")
 
 # Задание 3: Напишите функцию-генератор, которая принимает предложение и возвращает слова по одному.
 # Обработайте исключение StopIteration
 
-
+def sentence_engine(sentence):
+    words = sentence.split()
+    for word in words:
+        yield word
+try:
+    for word1 in sentence_engine("What a sunny day innit?"):
+        print(word1)
+except StopIteration:
+    print("Iteration complete")
