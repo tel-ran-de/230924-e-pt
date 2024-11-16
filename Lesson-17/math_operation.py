1cfdf055091e3b764bcccf40ceeee2e57e0b8f41
Viktor Yerokov <pokervipdonvip@gmail.com>
2024-11-16 10:37:27 +0100

    урок 21 удаление лишних файлов, предыдущих уроков

--- Lesson-17/math_operation.py
+++
@@ -1,9 +0,0 @@
-def multiply(a, b):
-    return a * b
-def sunstract(a, b):
-    return a - b
-def divide(a, b):
-    return a / b
-
-if __name__ == "__main__":
-    print("Модуль запущен напрямую")
