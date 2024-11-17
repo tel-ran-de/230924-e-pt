2bf18e8a4769f51a20e40e3cfb42ea20f34734a2
YuliiaRyzhanina <yryzhanina@gmail.com>
2024-11-17 21:31:19 +0200

    HW21 выполнен

--- Lesson-21/homework.py
+++ Lesson-21/homework.py
@@ -1,5 +1,5 @@
 # Тема: Итераторы и генераторы. Функции-генераторы. Выражения-генераторы
-
+import re
 # Задание 1: Напишите функцию, которая создает итератор, возвращающий числа от 1 до заданного числа `n`.
 # Обработайте исключение StopIteration
 # def number_generator(n):
@@ -17,9 +17,30 @@
 # Задание 2: Напишите выражение-генератор, которое возвращает квадраты чисел от 0 до 10.
 # Обработайте исключение StopIteration
 
+# squer_gen = (i*i for i in range(11))
+# try:
+#     for i in squer_gen:
+#         print(i)
+# except StopIteration:
+#      print("Итерация завершена")
+
+
 
 # Задание 3: Напишите функцию-генератор, которая принимает предложение и возвращает слова по одному.
 # Обработайте исключение StopIteration
+# def word_gen(text):
+#     words = text.split()
+#     for word in words:
+#         yield word
+# try:
+#     text = "Hello Python this is Yuliia"
+#     gen = word_gen(text)
+#     while True:
+#         word = next(gen)
+#         print(word)
+# except  StopIteration:
+#     print("Все слова были возвращены.")
+
 
 
 # Тема: Генераторы и встроенные функции
@@ -27,18 +48,34 @@
 # Задача 1: Генератор и функция set()
 # Задание: Напишите генератор, который возвращает числа от 1 до 10, но если число четное, возвратите его удвоенным.
 # Используйте функцию set(), чтобы преобразовать результат генератора в множество и выведите его.
-
-
+#
+# def number_generator(n):
+#     for i in range(1,n+1):
+#      if i%2 == 0:
+#          i*= 2
+#      yield i
+# gen = number_generator(10)
+# print(set(gen))
 # Задача 2: Генератор и функция sum()
 # Задание: Напишите генератор, который возвращает числа от 1 до 20, кратные 3. Используйте функцию sum(),
 # чтобы найти сумму всех этих чисел и выведите результат.
-
-
+# def number_generator(n):
+#     for i in range(1,n+1):
+#      if i%3 == 0:
+#       yield i
+# gen = number_generator(20)
+# print(sum(gen))
 # Задача 3: Генератор и функции min() и max()
 # Задание: Напишите генератор, который возвращает длины слов в заданной строке. Используйте функции min() и max(),
 # чтобы найти минимальную и максимальную длину слов и выведите их.
 # sentence = "Write a generator that returns word lengths from a given sentence"
-
+# def func(sentence):
+#     for i in sentence.split():
+#         yield len(i)
+# gen = func(sentence)
+# print(min(gen))
+# gen = func(sentence)
+# print(max(gen))
 
 # Тема: Генераторы и файлы
 
@@ -81,3 +118,12 @@
 # Задача 3: Поиск строк, содержащих числа
 # Создайте генератор, который читает строки из файла и возвращает только те строки, которые содержат числа.
 # Файл: data.txt
+# def read_file(file_path):
+#     with open(file_path, 'r') as file:
+#         for line in file:
+#             if re.search(r'\d', line):
+#                 yield line.strip()
+# file_path = 'text_files/data.txt'
+# gen = read_file(file_path)
+# for line in gen:
+#     print(line)
